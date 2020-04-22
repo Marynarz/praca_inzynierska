b20005c0e47d8d2b0d9efe4c9aeaf4a15eab4bdc
Wojtek <nwojtek@gmail.com>
2020-04-22 21:21:26 +0200

    poprawki w file handlingu

--- gui_tools/FileValidator.py
+++ gui_tools/FileValidator.py
@@ -5,16 +5,25 @@ from gui_tools import logger
 class FileValidator(object):
     # value = (x, y)
     values = []
+    FNAME_PATTERN = 'FileValidator.%s'
 
     def __init__(self, entry_point):
         self.entry_point = entry_point
         self.log = logger.Logger('FileValidator_' + str(self))
 
     def file_to_validate(self, source_file):
+        fname = self.FNAME_PATTERN % 'file_to_validate'
         if '.txt' in source_file:
-            self.validate_txt_file(source_file)
+            self.log.write_log(app_defs.INFO_MSG, '%s: TXT file chosen to validate: {%s}' % (fname, source_file))
+            try:
+                self.validate_txt_file(source_file)
+            except Exception as e:
+                self.log.write_log(app_defs.ERROR_MSG, '%s: Exception while handling file: %s, exception details {%s}'
+                                   % (fname, source_file, e))
+                return app_defs.UNABLE_TO_OPEN_FILE
             return app_defs.NOERROR
         else:
+            self.log.write_log(app_defs.INFO_MSG, '%s: Unknown file type for file: {%s}' % (fname, source_file))
             return app_defs.UNKNOWN_FILE_TYPE
 
     def validate_txt_file(self, source_file):
@@ -24,7 +33,7 @@ class FileValidator(object):
                     tmp_str = line.split(' ')
                     self.values.append((int(tmp_str[0]), int(tmp_str[1])))
         except Exception as e:
-            print(e)
+            raise e
 
     def get_values(self):
         return self.values
--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QMenuBar, QMainWindow, QAction, qApp, QGridLayout,\
-    QFileDialog
+    QFileDialog, QMessageBox, QMenu
 from PlotsCanvases import MplCanvas
 from gui_tools import FileValidator, logger
 import app_defs
 
 
-class PlotCompareMain(QMainWindow):
+class PlotCompareMain(QWidget):
     def __init__(self):
         super().__init__()
         try:
@@ -28,23 +28,22 @@ class PlotCompareMain(QMainWindow):
         open_file.setShortcut('Ctrl+O')
         open_file.triggered.connect(self.open_file_window)
 
-        menu = self.menuBar()
+        menu = QMenuBar()
 
         file_opt = menu.addMenu('&File')
-        file_opt.addAction(exit_action)
         file_opt.addAction(open_file)
+        file_opt.addAction(exit_action)
 
-
-        #plots
+        # plots
         mat_plot_lib_canvas = MplCanvas.MplCanvas(parent=self, x=10, y=10, dpi=100)
         mat_plot_lib_canvas.axes.plot([0,1,2,3,4], [10,1,20,3,40])
 
         main_layout = QGridLayout()
         main_layout.addWidget(mat_plot_lib_canvas)
-        gen_widget = QWidget()
-        #main window
-        gen_widget.setLayout(main_layout)
-        self.setCentralWidget(gen_widget)
+        main_layout.addWidget(menu)
+
+        # main window
+        self.setLayout(main_layout)
         self.setWindowTitle('Praca inzynierska - W. Niedzielski - 2020')
         self.show()
 
@@ -57,7 +56,15 @@ class PlotCompareMain(QMainWindow):
             ret = txt_file_points.file_to_validate(file_to_open)
 
         if ret != app_defs.NOERROR:
-            self.log.write_log(app_defs.WARNING_MSG, 'Unable to open or validate file, please see log file!')
+            if ret == app_defs.UNKNOWN_FILE_TYPE:
+                self.log.write_log(app_defs.WARNING_MSG, 'Unknown file type! File: %s' % file_to_open)
+                _ = QMessageBox.information(self, 'Unknown file type',
+                                            'File: %s cannot be open. File type unknown.' % file_to_open,
+                                            QMessageBox.Ok, QMessageBox.Ok)
+            elif ret == app_defs.UNABLE_TO_OPEN_FILE:
+                _ = QMessageBox.warning(self, 'Unknown file type',
+                                        'File: %s unable to open. Please see log file!' % file_to_open,
+                                        QMessageBox.Ok, QMessageBox.Ok)
 
 
 if __name__ == '__main__':
