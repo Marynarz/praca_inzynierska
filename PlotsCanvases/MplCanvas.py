e231a353a58dad175ced2bd725022a2f8ef2209b
Wojtek <nwojtek@gmail.com>
2020-05-01 11:24:25 +0200

    kolejny commit, bedzie konflikt

--- PlotsCanvases/MplCanvas.py
+++ PlotsCanvases/MplCanvas.py
@@ -6,11 +6,15 @@ from matplotlib.figure import Figure
 
 
 class MplCanvas(FigureCanvasQTAgg):
+
     def __init__(self, parent=None, x=10, y=10, dpi=100):
         figure = Figure(figsize=(x, y), dpi=dpi)
         self.axes = figure.add_subplot(111)
         FigureCanvasQTAgg.__init__(self, figure)
         self.setParent(parent)
+        self.now_x = [x]
+        self.now_y = [y]
+        self.grid = False
 
         FigureCanvasQTAgg.setSizePolicy(self,
                                    QtWidgets.QSizePolicy.Expanding,
@@ -20,5 +24,14 @@ class MplCanvas(FigureCanvasQTAgg):
     def update_canvas(self, x, y):
         self.axes.cla()
         self.axes.plot(x, y)
+        self.draw()
+        self.now_x = x
+        self.now_y = y
+
+    def show(self):
+        self.grid = not self.grid
+        self.axes.cla()
+        self.axes.plot(self.now_x, self.now_y)
+        self.axes.grid(self.grid)
         self.draw()
 
