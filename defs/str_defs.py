3e08da25e4b0f520067e6a407daa5ded0008e835
Wojciech Niedzielski <nwojtek@gmail.com>
2020-12-28 10:20:16 +0100

    internatiolization

--- defs/str_defs.py
+++ defs/str_defs.py
@@ -1,7 +1,15 @@
 # main window title
 MAIN_WINDOW_TITLE = {'pl': 'PRACA INZYNIERSKA', 'en': 'ENG PROJECT'}
 
+# internationalization
+LANG_PL = 'pl'
+LANG_ENG = 'en'
+
 # menu defs
 MENU = '&Menu'
-NEW_APP = {'pl': '&Nowy', 'en': '&New'}
-EXIT_APP = {'pl': '&Wyjscie', 'en': '&Exit'}
+NEW_APP = {LANG_PL: '&Nowy', LANG_ENG: '&New'}
+EXIT_APP = {LANG_PL: '&Wyjscie', LANG_ENG: '&Exit'}
+SETTINGS_MENU = {LANG_PL: '&Ustawienia', LANG_ENG: '&Settings'}
+LANG_CHANGE = {LANG_PL: '&Jezyk', LANG_ENG: '&Language'}
+POLISH = {LANG_PL: '&Polski', LANG_ENG: '&Polish'}
+ENGLISH = {LANG_PL: '&Angielski', LANG_ENG: '&English'}
--- main_v2.py
+++ main_v2.py
@@ -1,6 +1,7 @@
 import sys
 
-from PyQt5.QtWidgets import QApplication, QMainWindow, QStatusBar
+from PyQt5.QtWidgets import QApplication, QMainWindow, QStatusBar, QGridLayout, QWidget, QAction
+from PyQt5.QtCore import QSettings
 from defs import str_defs, app_defs
 from gui_tools import logger
 
@@ -13,8 +14,23 @@ class MainWindow(QMainWindow):
         except Exception as e:
             print(e)
             sys.exit(0)
+        self.settings = QSettings('wnie', 'praca_inzynierska')
+        if self.settings.contains('lang'):
+            self.language = self.settings.value('lang')
+        else:
+            # default english
+            self.language = str_defs.LANG_ENG
+
         self.log.write_log(app_defs.INFO_MSG, 'Hello main gui\t--\tV2.00')
-        self.setWindowTitle(str_defs.MAIN_WINDOW_TITLE['pl'])
+        self.log.write_log(app_defs.ERROR_MSG, 'lang: %s, type: %s' % (self.language, type(self.language)))
+        self.setWindowTitle(str_defs.MAIN_WINDOW_TITLE[self.language])
+
+        # Setting central widget
+        self.general_layout = QGridLayout()
+        self._central_widget = QWidget(self)
+        self.setCentralWidget(self._central_widget)
+        self._central_widget.setLayout(self.general_layout)
+
         self._create_menu()
         self._create_status_bar()
 
@@ -22,11 +38,30 @@ class MainWindow(QMainWindow):
         self.menu = self.menuBar().addMenu(str_defs.MENU)
         # new
 
-        self.menu.addAction(str_defs.NEW_APP['pl'], self.reset)
+        self.menu.addAction(str_defs.NEW_APP[self.language], self.reset)
 
         # exit section
         self.menu.addSeparator()
-        self.menu.addAction(str_defs.EXIT_APP['pl'], self.close)
+        self.exit_action = QAction(str_defs.EXIT_APP[self.language], self)
+        self.exit_action.setShortcut('Ctrl+Q')
+        self.menu.addAction(self.exit_action)
+        self.exit_action.triggered.connect(self.close)
+
+        # settings menu
+        self.sets = self.menuBar().addMenu(str_defs.SETTINGS_MENU[self.language])
+
+        # internationalization
+        self.langs = self.sets.addMenu(str_defs.LANG_CHANGE[self.language])
+
+        # pl
+        change_lang_action_pl = QAction(str_defs.POLISH[self.language], self)
+        self.langs.addAction(change_lang_action_pl)
+        change_lang_action_pl.triggered.connect(lambda x: self.set_lang(str_defs.LANG_PL))
+
+        # eng
+        change_lang_action_eng = QAction(str_defs.ENGLISH[self.language], self)
+        self.langs.addAction(change_lang_action_eng)
+        change_lang_action_eng.triggered.connect(lambda x: self.set_lang(str_defs.LANG_ENG))
 
     def _create_status_bar(self):
         status = QStatusBar()
@@ -35,6 +70,9 @@ class MainWindow(QMainWindow):
 
     def reset(self):
         pass
+
+    def set_lang(self, lang):
+        self.settings.setValue('lang', lang)
 
 
 if __name__ == '__main__':
