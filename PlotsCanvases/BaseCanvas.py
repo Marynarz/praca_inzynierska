d6cf63aaada268a81666d830a8074cf34e06bf77
Wojciech Niedzielski <nwojtek@gmail.com>
2020-12-29 08:50:52 +0100

    exception handling
    exception handling in abstract class

--- PlotsCanvases/BaseCanvas.py
+++ PlotsCanvases/BaseCanvas.py
@@ -4,26 +4,16 @@ class BaseCanvas(ABC)
     
 	@abstractmethod
 	def update_plot(self):
-	    pass
+	    raise NotImplementedError('Not implemented yet')
 	
 	@abstractmethod
 	def show(self):
-	    pass
+	    raise NotImplementedError('Not implemented yet')
 		
 	@abstractmethod
 	def set_grid(self):
-	    pass
+	    raise NotImplementedError('Not implemented yet')
 		
 	@abstractmethod
 	def set_line(self):
-	    pass
-
-class NotImplementedError(Exception):
-    def __init__(self, error_code, error_msg, exit_code):
-	    self.error_code = error_code
-		self.error_msg = error_msg
-		self.exit_code = exit_code
-	
-	def __str__(self):
-	    return 'NotImplementedError: error_code : {0!s}, error_msg : {1}, exit_code : {2!s}'.format(self.error_code, self.error_msg, self.exit_code)
-
+	    raise NotImplementedError('Not implemented yet')
