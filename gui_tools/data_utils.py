65683a733a733eab50b11afa7627a704acaa1d80
Wojciech Niedzielski <nwojtek@gmail.com>
2021-01-17 13:03:43 +0100

    dodanie data utils, nie dziala liczenie radianow

--- gui_tools/data_utils.py
+++ gui_tools/data_utils.py
@@ -1,9 +1,26 @@
-import pandas as pd
+import math
 
 
-def percent_to_radius(data):
-    pass
+def percent_to_radius(data, column_name):
+    data['Radius'] = math.radians((data[column_name] / 100) * 360)
+    return data
 
 
-def dataframe_to_radius(data):
-    pass
+def dataframe_to_radius(data, column_name):
+    data['Percent'] = (data[column_name] / data[column_name].sum()) * 100
+    return data
+
+
+##
+# @brief filter_negative_numbers - function to filter and remove negative numbers from dataframe
+#
+# @params
+#     data - data to be filtered                                  (pandas.DataFrame)
+#     column_name - name of column whichc should be filtered      (pandas.DataFrame.column.name)
+#
+# @return
+#     pandas.Dataframe - filtered dataframe                       (pandas.DataFrame)
+#
+def filter_negative_numbers(data, column_name):
+    filtered = data[column_name] > 0
+    return data[filtered]
