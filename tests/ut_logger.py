86e2475723e663b30714ca579905dc3f4b1e5fea
Wojciech Niedzielski <nwojtek@gmail.com>
2021-01-20 18:51:36 +0100

    unittesty

--- tests/ut_logger.py
+++ tests/ut_logger.py
@@ -35,6 +35,35 @@ class UtLogger(unittest.TestCase):
                 self.assertEqual(messages[idx], line)
                 idx += 1
 
+    @mock.patch('time.time', mock.MagicMock(return_value=666))
+    def test02_logger_appending(self):
+        messages = ('Start logging at: 666 from: unit_test\n',
+                    '%s: Test info message\n' % app_defs.INFO_MSG,
+                    '%s: Test warning message\n' % app_defs.WARNING_MSG,
+                    '%s: Test error message\n' % app_defs.ERROR_MSG,
+                    'Start logging at: 666 from: unit_test\n',
+                    '%s: Appended info message\n' % app_defs.INFO_MSG,
+                    '%s: Appended warning message\n' % app_defs.WARNING_MSG,
+                    '%s: Appended error message\n' % app_defs.ERROR_MSG)
+        test_logger = logger.Logger('unit_test')
+        test_logger.write_log(app_defs.INFO_MSG, 'Test info message')
+        test_logger.write_log(app_defs.WARNING_MSG, 'Test warning message')
+        test_logger.write_log(app_defs.ERROR_MSG, 'Test error message')
+
+        test_append_log = logger.Logger('unit_test', append=True)
+        test_append_log.write_log(app_defs.INFO_MSG, 'Appended info message')
+        test_append_log.write_log(app_defs.WARNING_MSG, 'Appended warning message')
+        test_append_log.write_log(app_defs.ERROR_MSG, 'Appended error message')
+
+        with open(self.log_dir) as f:
+            idx = 0
+            for line in f:
+                print(line)
+                self.assertEqual(messages[idx], line)
+                idx += 1
+
+            self.assertEqual(idx, len(messages))
+
 
 if __name__ == '__main__':
     unittest.main()
