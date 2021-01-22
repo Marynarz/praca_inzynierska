f2543d6c85f7f1e3a842c15f0871d0c4ec3a03bf
Wojciech Niedzielski <nwojtek@gmail.com>
2021-01-22 22:15:26 +0100

    dodanie ut dal filevalidatora

--- tests/ut_FileValidator.py
+++ tests/ut_FileValidator.py
@@ -1,6 +1,9 @@
 import sys
+import os
 import unittest
-sys.path.append('..')
+dir_path = os.path.dirname(os.path.realpath(__file__))
+parent_dir_path = os.path.abspath(os.path.join(dir_path, os.pardir))
+sys.path.insert(0, parent_dir_path)
 
 from defs import app_defs
 from gui_tools import FileValidator
@@ -8,7 +11,8 @@ from gui_tools import FileValidator
 
 class FileValidatorTest(unittest.TestCase):
     def setUp(self):
-        pass
+        self.returned_data = ([1, 3, 10, -1], [2, 2, 3, -1])
+        self.headers = ['x', 'y']
 
     def tearDown(self):
         pass
@@ -26,13 +30,61 @@ class FileValidatorTest(unittest.TestCase):
         self.assertEqual(returned, app_defs.UNKNOWN_FILE_TYPE)
 
     def test03_txt_file_validate(self):
-        returned_list = [(-1, -1), (1, 2), (3, 2), (10, 3)]
         test_class = FileValidator.FileValidator('test')
 
-        returned = test_class.file_to_validate('ut_data/test_data.txt')
-
+        returned = test_class.file_to_validate('tests/ut_data/test_data.txt')
         self.assertEqual(returned, app_defs.NOERROR)
-        self.assertListEqual(returned_list, test_class.get_values())
+
+        for i in range(2):
+            self.assertListEqual(self.returned_data[i], test_class.get_values()[i].tolist())
+
+    def test04_csv_file_validate(self):
+        test_class = FileValidator.FileValidator('test')
+
+        returned = test_class.file_to_validate('tests/ut_data/test_data.csv')
+        self.assertEqual(returned, app_defs.NOERROR)
+
+        for i in range(2):
+            self.assertListEqual(self.returned_data[i], test_class.get_values()[i].tolist())
+
+    def test05_json_file_validate(self):
+        test_class = FileValidator.FileValidator('test')
+
+        returned = test_class.file_to_validate('tests/ut_data/test_data.json')
+        self.assertEqual(returned, app_defs.NOERROR)
+
+        for i in range(2):
+            self.assertListEqual(self.returned_data[i], test_class.get_values()[i].tolist())
+
+    def test06_txt_file_with_header_validate(self):
+        test_class = FileValidator.FileValidator('test')
+
+        returned = test_class.file_to_validate('tests/ut_data/test_data_with_header.txt')
+        self.assertEqual(returned, app_defs.NOERROR)
+        self.assertListEqual(self.headers, test_class.get_values().columns.tolist())
+
+        for i in range(2):
+            self.assertListEqual(self.returned_data[i], test_class.get_values()[self.headers[i]].tolist())
+
+    def test07_csv_file_with_header_validate(self):
+        test_class = FileValidator.FileValidator('test')
+
+        returned = test_class.file_to_validate('tests/ut_data/test_data_with_header.csv')
+        self.assertEqual(returned, app_defs.NOERROR)
+        self.assertListEqual(self.headers, test_class.get_values().columns.tolist())
+
+        for i in range(2):
+            self.assertListEqual(self.returned_data[i], test_class.get_values()[self.headers[i]].tolist())
+
+    def test08_json_file_with_header_validate(self):
+        test_class = FileValidator.FileValidator('test')
+
+        returned = test_class.file_to_validate('tests/ut_data/test_data_with_header.json')
+        self.assertEqual(returned, app_defs.NOERROR)
+        self.assertListEqual(self.headers, test_class.get_values().columns.tolist())
+
+        for i in range(2):
+            self.assertListEqual(self.returned_data[i], test_class.get_values()[self.headers[i]].tolist())
 
 
 if __name__ == '__main__':
