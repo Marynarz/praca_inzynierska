e954c37b86babdcef4a361c168ca4fdde264df0d
Wojciech Niedzielski <nwojtek@gmail.com>
2021-03-28 17:37:29 +0200

    json loader dziala z obsluga bledow

--- gui_tools/json_url_loader.py
+++ gui_tools/json_url_loader.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QWidget, QFormLayout, QLabel, QLineEdit
+from PyQt5.QtWidgets import QWidget, QFormLayout, QLineEdit, QPushButton, QMessageBox
 import validators
 import pandas as pd
 
@@ -9,20 +9,40 @@ class JsonUrlOpen(QWidget):
         self.dv = data_viewer
         layout = QFormLayout()
         self.url_set = QLineEdit('Type url here')
-        self.url_set.textChanged.connect(self.download_json)
+        btn_set_url = QPushButton('Set url')
+        btn_set_url.clicked.connect(self.download_json)
         layout.addRow('Url:', self.url_set)
+        layout.addRow(btn_set_url)
         self.setLayout(layout)
+
+    def download_json(self):
+        text = self.url_set.text()
+        valid, url = self.validate_url(text)
+        if not valid:
+            self._err_msg_box('Wrong url', 'Url validation not passed', url + ' is not valid url')
+        else:
+            try:
+                data = pd.read_json(url)
+                self.dv.set_data(data)
+            except Exception:
+                self._err_msg_box('Url not working', 'Your url not working properly', 'Double check it')
+            else:
+                self.close()
 
     @staticmethod
     def validate_url(url):
-        return validators.url(url)
+        if not validators.url(url):
+            url = 'http://' + url
+            return validators.url(url), url
+        return True, url
 
-    def download_json(self, text):
-        if not self.validate_url(text):
-            print("Wrong JSON url!")
-        else:
-            data = pd.read_json(text)
-            data = pd.json_normalize(data)
-            print(data)
-            self.dv.set_data(data)
+    @staticmethod
+    def _err_msg_box(title, text, additional_txt = ''):
+        not_valid_msg = QMessageBox()
+        not_valid_msg.setIcon(QMessageBox.Warning)
+        not_valid_msg.setWindowTitle(title)
+        not_valid_msg.setText(text)
+        not_valid_msg.setInformativeText(additional_txt)
+        not_valid_msg.setStandardButtons(QMessageBox.Ok)
+        _ = not_valid_msg.exec()
 
